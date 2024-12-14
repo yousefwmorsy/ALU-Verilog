a456cfd3453e5b105a88f7884fa6d9f4ff77b9ab
Yousef Morsy <yousefwmorsy@gmail.com>
2024-12-14 12:49:20 +0200

    readded flags

--- rem.v
+++ rem.v
@@ -1,11 +1,11 @@
 module rem (
     input [2:0] A, [2:0] B,
-    output [2:0] R
+    output DZF, ZF , SF, [2:0] R
 );
 assign DZF =(!B[1])&(!B[0]);
 assign R[0]=!DZF&(A[0]&B[1]&!(A[1]&B[0]));
 assign R[1]=!DZF&(A[1]&(!A[0])&B[1]&B[0]);
 assign R[2]=!DZF&(A[2]);
-assign SF = A[2];
-assign ZF = (!R[0])&(!R[1]);
+assign ZF = (!R[0])&(!R[1])&!DZF;
+assign SF = A[2]&!ZF;
 endmodule
